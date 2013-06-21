ec74051dec584f76817307e5b5b6dd925e53cba8
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2013-06-21 20:19:53 +0200

    Added Plots.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-yycflood
+#yycflood River Data
 ========
 
 #yycflood 2013 River Data Plots
+
+This R analysis pulls river flow and height data from Alberta Environment, and generates a plot for both the Bow & Elbow rivers showing flow-rate trends and normalized water levels (relative to pre-flood levels). 
+
+![yycflood](yycflood_riverplots.png)
---
+++ yycflood.R
@@ -0,0 +1,55 @@
+# AB River Data
+
+# Packages
+require(data.table)
+require(ggplot2)
+require(reshape)
+require(gridExtra)
+
+# Set-up Data Sources
+stations<-data.frame()
+stations<-rbind(stations,data.frame(river="Bow River",station="Lake Louise",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BA001.csv"))
+stations<-rbind(stations,data.frame(river="Bow River",station="Banff",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BB001.csv"))
+stations<-rbind(stations,data.frame(river="Bow River",station="Cochrane",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BH005.csv"))
+stations<-rbind(stations,data.frame(river="Bow River",station="Calgary",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BH004.csv"))
+stations<-rbind(stations,data.frame(river="Bow River",station="Carseland",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BM002.csv"))
+stations<-rbind(stations,data.frame(river="Elbow River",station="Bragg Creek",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BJ004.csv"))
+stations<-rbind(stations,data.frame(river="Elbow River",station="Sarcee",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BJ010.csv"))
+stations<-rbind(stations,data.frame(river="Elbow River",station="Glenmore Below Dam",url="http://www.environment.alberta.ca/apps/Basins/data/text/river/05BJ001.csv"))
+
+# Load Data from URLs
+stations$url<-as.character(stations$url)
+raw.data<-NULL
+raw.data<-data.frame()
+for(i in 1:nrow(stations))
+{
+  raw.data<-rbind(raw.data,
+                  cbind(river=stations$river[i],station=stations$station[i],read.table(file=stations$url[i],sep=",",skip=22,header=TRUE,fill=TRUE,blank.lines.skip=TRUE,stringsAsFactors=FALSE)))
+}
+
+# Clean-up & Format Data
+colnames(raw.data)<-c("river","name","station.no","date","water.level.m","flow.m3s")
+
+raw.data<-subset(raw.data,date!="")
+
+raw.data$date<-as.POSIXct(strptime(raw.data$date,format="%Y-%m-%d %H:%M:%S"),tz="MST")
+raw.data$flow.m3s<-as.numeric(raw.data$flow.m3s)
+raw.data$water.level.m<-as.numeric(raw.data$water.level.m)
+
+raw.data<-data.table(raw.data)
+raw.data[,norm.factor:=min(water.level.m),by=c("name")]
+raw.data[,norm.water.level:=water.level.m/norm.factor]
+raw.data<-data.frame(raw.data)
+
+# Pivot Data for plots
+colnames(raw.data)<-c("river","name","station.no","date","Water Level (m)","Flow Rate (m3/s)","norm.factor","Normalized Water Level")
+melted.data<-melt(raw.data,id.vars=c("river","name","station.no","date"))
+
+# Filter data & Build Plot
+plot.data<-subset(melted.data,date>=as.POSIXct("2013-06-19 12:00:00",tz="MST") & variable %in% c("Flow Rate (m3/s)","Normalized Water Level"))
+p1<-ggplot(data=plot.data,aes(x=date,y=value))+geom_line(lwd=0.75,aes(color=name))+xlab("Date")+ylab("")+ggtitle("River Flow and Water Level - #yycflood")+facet_grid(river~.)+theme_bw()+theme(legend.position="bottom")+scale_color_discrete(name="")+facet_grid(variable~river,scales="free")
+
+# Save to Image File
+png("yycflood_riverplots.png",width=800,height=494,res=96)
+grid.arrange(p1,nrow=1,sub = textGrob(paste0("Last Update: ",max(raw.data$date),",  Data: Alberta Environment "), x=1, hjust=1, vjust=0,gp=gpar(fontsize=12)))
+dev.off()
