5f0d63780397371f3cd0a8721b2af7222b70730f
Mike Birdgeneau <mike.birdgeneau@gmail.com>
2013-06-22 04:39:06 +0200

    Data Update.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-Alberta Bow River Basin: Flow & Water Level Data (\#yycflood)
+Alberta Bow River Basin: Flow, Reservoir Storage & Water Level Data (\#yycflood)
 ========
 
 ### Summary
 
-This R analysis pulls river flow and height data from Alberta Environment, and generates a plot for both the Bow & Elbow rivers showing flow-rate trends and normalized water levels (relative to pre-flood levels). 
+This R analysis pulls river flow and height data from Alberta Environment, and generates a plot for both the Bow & Elbow rivers showing flow-rate trends and normalized water levels (relative to pre-flood levels).
+It also pulls reservoir water level and storage volumes from dam controlled reservoirs / lakes. 
 
 ### Flow & Water Level Plots
 
-![yycflood](yycflood_riverplots.png)
+![River Flows and Water Level yycflood](yycflood_riverplots.png)
+
+![Reservoir Storage and Water Levels yycflood](yycflood_reservoirplots.png)
 
 **Note:** Real-time data is subject to revision.
 
--- yycflood.R
+++ yycflood.R
@@ -66,11 +66,16 @@ if(exists("old.res.data"))
 # Remove Duplicates
 raw.data<-raw.data[!duplicated(raw.data),]
 save(raw.data,file="river_data.Rda")
+raw.res.data<-raw.res.data[!duplicated(raw.res.data),]
+save(raw.res.data,file="reservoir_data.Rda")
 
 # Clean-up & Format Data
 colnames(raw.data)<-c("river","name","station.no","date","water.level.m","flow.m3s")
+colnames(raw.res.data)<-c("reservoir","station.no","date","water.level.m","storage.m3")
+
 
 raw.data<-subset(raw.data,date!="")
+raw.res.data<-subset(raw.res.data,date!="")
 
 raw.data$date<-as.POSIXct(strptime(raw.data$date,format="%Y-%m-%d %H:%M:%S"),tz="MST")
 raw.data$flow.m3s<-as.numeric(raw.data$flow.m3s)
@@ -81,17 +86,36 @@ raw.data[,norm.factor:=min(water.level.m),by=c("name")]
 raw.data[,norm.water.level:=water.level.m/norm.factor]
 raw.data<-data.frame(raw.data)
 
+raw.res.data$date<-as.POSIXct(strptime(raw.res.data$date,format="%Y-%m-%d %H:%M:%S"),tz="MST")
+raw.res.data$storage.m3<-as.numeric(raw.res.data$storage.m3)
+raw.res.data$water.level.m<-as.numeric(raw.res.data$water.level.m)
+
+raw.res.data<-data.table(raw.res.data)
+raw.res.data[,norm.factor:=min(water.level.m),by=c("reservoir")]
+raw.res.data[,norm.water.level:=water.level.m/norm.factor]
+raw.res.data<-data.frame(raw.res.data)
+
 # Pivot Data for plots
 colnames(raw.data)<-c("river","name","station.no","date","Water Level (m)","Flow Rate (m3/s)","norm.factor","Normalized Water Level")
 melted.data<-melt(raw.data,id.vars=c("river","name","station.no","date"))
+
+colnames(raw.res.data)<-c("reservoir","station.no","date","Water Level (m)","Storage (m3)","norm.factor","Normalized Water Level")
+melted.res.data<-melt(raw.res.data,id.vars=c("reservoir","station.no","date"))
 
 # Filter data & Build Plot
 plot.data<-subset(melted.data,date>=as.POSIXct("2013-06-19 18:00:00",tz="MST") & variable %in% c("Flow Rate (m3/s)","Normalized Water Level"))
 p1<-ggplot(data=plot.data,aes(x=date,y=value))+geom_line(lwd=0.75,aes(color=name))+xlab("Date")+ylab("")+ggtitle("River Flow and Water Level - #yycflood\n")+facet_grid(river~.)+theme_bw()+theme(legend.position="bottom")+scale_color_discrete(name="")+facet_wrap(variable~river,scales="free")+theme(axis.text.x = element_text(angle = -90, hjust = 1))
 
+plot.res.data<-subset(melted.res.data,date>=as.POSIXct("2013-06-15 18:00:00",tz="MST") & variable %in% c("Storage (m3)","Normalized Water Level"))
+p2<-ggplot(data=plot.res.data,aes(x=date,y=value))+geom_line(lwd=0.75,aes(color=reservoir))+xlab("Date")+ylab("")+ggtitle("Reservoir Storage and Water Level - #yycflood\n")+facet_grid(variable~.,scales="free")+theme_bw()+theme(legend.position="bottom")+scale_color_discrete(name="")+theme(axis.text.x = element_text(angle = -90, hjust = 1))
+
 # Save to Image File
 png("yycflood_riverplots.png",width=1400,height=865,res=96)
 grid.arrange(p1,nrow=1,sub = textGrob(paste0("Last Update: ",max(raw.data$date),"  -  Source: Alberta Environment (real-time data subject to revision) "), x=0.5, hjust=0.5, vjust=0,gp=gpar(fontsize=12)))
+dev.off()
+
+png("yycflood_reservoirplots.png",width=1400,height=865,res=96)
+grid.arrange(p2,nrow=1,sub = textGrob(paste0("Last Update: ",max(raw.res.data$date),"  -  Source: Alberta Environment (real-time data subject to revision) "), x=0.5, hjust=0.5, vjust=0,gp=gpar(fontsize=12)))
 dev.off()
 
 # Output newest date
